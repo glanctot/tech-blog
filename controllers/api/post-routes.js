86c0281615e3a58a873c4b69752b1a2c194c9ce0
glanctot <gregory.lanctot28@gmail.com>
2022-01-14 11:05:06 -0600

    comments added

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,13 +1,20 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const { Post, User, Comment } = require('../../models');
 
 // get all posts
 router.get('/', (req, res) => {
-    console.log('=========');
     Post.findAll({
-        attributes: ['id', 'title', 'created_at'],
         order:[['created_at', 'DESC']],
+        attributes: ['id', 'title', 'created_at'],
         include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
             {
                 model: User,
                 attributes: ['username']
@@ -29,6 +36,14 @@ router.get('/:id', (req, res) => {
         },
         attributes: ['id', 'title', 'created_at'],
         include: [
+            {
+                model: Comment,
+                attributes: ['id','comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
             {
                 model: User,
                 attributes: ['username']
@@ -100,4 +115,5 @@ router.delete('/:id', (req, res) => {
         res.status(500).json(err);
     })
 })
+
 module.exports = router;
